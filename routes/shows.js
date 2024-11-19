21e05fffedce159da67546a0f7b4ffad9707a42a
Robyn <Robyn796@mac.com>
2024-11-19 12:25:24 -0700

    adding routes in shows.js crud

--- routes/shows.js
+++ routes/shows.js
@@ -7,5 +7,10 @@ router.get('/', showsController.getAll);
 
 router.get('/:id', showsController.getSingle);
 
+router.post('/', showsController.createShow);
+
+router.put('/:id', showsController, updateShow);
+
+router.delete('/:id', showsController, deleteShow);
 
 module.exports = router;

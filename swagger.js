dcf78ca9ec02013734afb14d693ae72117bdcf93
Robyn <Robyn796@mac.com>
2024-11-25 22:54:18 -0700

    swagger ui

--- swagger.js
+++ swagger.js
@@ -3,15 +3,15 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
     info: {
-        title: 'Users Api',
-        description: 'Users Api'
+        title: 'Shows Api',
+        description: 'Shows Api'
     },
     host: 'localhost:3000',
     schemes: ['http', 'https']
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const endpointsFiles = ['./routes/shows.js'];
 
 // this will generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
